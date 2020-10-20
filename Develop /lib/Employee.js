4b2709e6356bff9ebf4d719c04194c61d195c407
Mary Kathryn <msmarykathrynholt@gmail.com>
2020-10-20 15:10:06 -0500

    initial

---
+++ Develop /lib/Employee.js
@@ -0,0 +1,14 @@
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email, github) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+    }
+  
+    printInfo() {
+      console.log(`This vehicle has ${this.numberOfWheels} wheels`);
+      console.log(`This vehicle has an id of ${this.id}`);
+    }
+  }
+  module.exports = Vehicle;
