41ef7f536de0c7f502b2ddbd089d41ad7f7a60a7
Mary Kathryn <msmarykathrynholt@gmail.com>
2020-10-20 15:31:07 -0500

    added engineer js file info for github and getrole

--- Develop /lib/Engineer.js
+++ Develop /lib/Engineer.js
@@ -4,6 +4,13 @@ const Employee = require("./Employee");
 class Engineer extends Employee {
     constructor(name, id, email, github) {
         super(name, id, email);
-        this.github
+        this.github = github;
     }
+
+    getgithub() {
+        return this.github
+      }
+    getRole() {
+        return "Engineer"
+      }
 }
